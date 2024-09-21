ac5ce953b86378b305b245edfff841b7c96f3c89
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-09-21 12:58:09 -0500

    .

--- src/main/java/co/edu/uniquindio/biblioteca/MainApp.java
+++ src/main/java/co/edu/uniquindio/biblioteca/MainApp.java
@@ -18,26 +18,22 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
 
-        // Antes de mostrar la pantalla de login, copiar el archivo Estudiantes.txt a una ubicación accesible
         copiarArchivoDeRecursos();
 
-        // Mostrar la pantalla de login
+
         showLoginScreen();
     }
 
-    // Método para copiar el archivo Estudiantes.txt desde el classpath a la carpeta del usuario
+
     private void copiarArchivoDeRecursos() throws IOException {
-        // Ruta donde copiar el archivo (en la carpeta del usuario)
         Path archivoDestino = Paths.get(System.getProperty("user.home"), "Estudiantes.txt");
 
-        // Verifica si el archivo de recursos existe
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("co/edu/uniquindio/biblioteca/archivos/Estudiantes.txt");
         if (inputStream == null) {
             System.out.println("El archivo Estudiantes.txt no fue encontrado en el classpath.");
             return;
         }
 
-        // Copia el archivo si no existe en la carpeta del usuario
         if (!Files.exists(archivoDestino)) {
             Files.copy(inputStream, archivoDestino, StandardCopyOption.REPLACE_EXISTING);
             System.out.println("Archivo Estudiantes.txt copiado a: " + archivoDestino.toString());
@@ -46,8 +42,6 @@ public class MainApp extends Application {
         }
     }
 
-
-    // Método para mostrar la pantalla de login
     public static void showLoginScreen() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
@@ -64,14 +58,14 @@ public class MainApp extends Application {
         primaryStage.show();
     }
 
-    // Método para mostrar la pantalla de inicio
+
     public static void mostrarInicio() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("Inicio.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
         primaryStage.setScene(scene);
     }
 
-    // Getters y setters para el primaryStage
+
     public static Stage getPrimaryStage() {
         return primaryStage;
     }
